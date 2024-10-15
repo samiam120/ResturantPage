aacceaaba330e4df304b95ab1800e95a864f6efd
samiam120 <samassefa3@gmail.com>
2024-10-14 21:26:37 -0700

    basic html structure and homepage

---
+++ src/home.js
@@ -0,0 +1,5 @@
+// export function contentText(){
+//     const content = document.querySelector('#content');
+//     content.textContent = "";
+//     content.textContent = "Here is some information about our resturant";
+// }
