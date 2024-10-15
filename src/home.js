5f6334148806aa994d8360200f49c401227f0f8d
samiam120 <samassefa3@gmail.com>
2024-10-15 11:56:21 -0700

    home content

--- src/home.js
+++ src/home.js
@@ -1,5 +1,28 @@
-// export function contentText(){
-//     const content = document.querySelector('#content');
-//     content.textContent = "";
-//     content.textContent = "Here is some information about our resturant";
-// }
+export function homeContent() {
+  const content = document.querySelector("#content");
+
+  const left = homeText();
+  const right = homeImg();
+
+  content.append(left, right);
+  return content;
+}
+
+function homeText() {
+  const leftSide = document.createElement("div");
+  leftSide.classList.add("left-side");
+
+  const p1 = document.createElement("p");
+  const p2 = document.createElement("p");
+  p1.textContent = "lorem";
+  p2.textContent = "second Lorem";
+
+  leftSide.append(p1, p2);
+  return leftSide;
+}
+
+function homeImg() {
+  const rightSide = document.createElement("div");
+  rightSide.classList.add("right-side");
+  return rightSide;
+}
